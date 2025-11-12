2ef25df43076885e169f58727250df7f3a1b7ab0
Sandro Martínez <virgosmp.mx@gmail.com>
2025-11-12 00:31:46 -0600

    ListingItems

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { use } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import { doc, updateDoc } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  collection,
+  query,
+  where,
+  orderBy,
+  getDocs,
+  deleteDoc,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import { GiFullMotorcycleHelmet } from "react-icons/gi";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import ListingItem from "../components/ListingItem";
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
   const [changeDetail, setChangeDetail] = React.useState(false);
   const [formData, setFormData] = React.useState({
     name: auth.currentUser.displayName,
@@ -21,22 +35,35 @@ export default function Profile() {
     navigate("/");
   }
   async function onSubmit() {
-      try {
-        if (auth.currentUser.displayName !== name) {
-          await updateProfile(auth.currentUser, {
-            displayName: name,
-          });
-  
-          const docRef = doc(db, "users", auth.currentUser.uid);
-          await updateDoc(docRef, {
-            name,
-          });
-        }
-        toast.success("Profile details updated");
-      } catch (error) {
-        toast.error("Could not update the profile details");
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
       }
+      toast.success("Profile details updated");
+    } catch (error) {
+      toast.error("Could not update the profile details");
     }
+  }
+  async function onDelete(listingID) {
+    if (window.confirm("Are you sure you want to delete?")) {
+      await deleteDoc(doc(db, "listings", listingID));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      );
+      setListings(updatedListings);
+      toast.success("Successfully deleted the listing");
+    }
+  }
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`);
+  }
   function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
@@ -44,6 +71,27 @@ export default function Profile() {
     }));
   }
   const { name, email } = formData;
+  useEffect(() => {
+    async function fetchUserListings() {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -102,6 +150,26 @@ export default function Profile() {
           </button>
         </div>
       </section>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold mb-6">
+              My Listings
+            </h2>
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </>
   );
 }
