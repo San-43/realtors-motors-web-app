5680feee80577e2ed49a7ed898d40b04ec5f23b1
Sandro Martínez <virgosmp.mx@gmail.com>
2025-11-12 03:10:01 -0600

    rent and sell pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PrivateRoute from "./components/PrivateRoute"
 import ForgotPassword from './pages/ForgotPassword'
 import Offers from './pages/Offers'
 import Listing from './pages/Listing'
+import Category from './pages/Category'
 import Header from './components/Header'
 import EditListing from './pages/EditListing'
 import CreateListing from './pages/CreateListing'
@@ -27,6 +28,7 @@ export default function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
           </Route>
