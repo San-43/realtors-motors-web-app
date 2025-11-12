5c19b9e792933a7413453b848eca8c605e4680b1
Sandro Martínez <virgosmp.mx@gmail.com>
2025-11-12 03:04:34 -0600

    offers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,7 +75,6 @@ export default function Home() {
     }
     fetchListings();
   }, []);
-  // Places for rent
   const [saleListings, setSaleListings] = useState(null);
   useEffect(() => {
     async function fetchListings() {
@@ -131,11 +130,11 @@ export default function Home() {
         {rentListings && rentListings.length > 0 && (
           <div className="m-2 mb-6">
             <h2 className="px-3 text-2xl mt-6 font-semibold">
-              Places for rent
+              Motorcycles for rent
             </h2>
             <Link to="/category/rent">
               <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
-                Show more places for rent
+                Show more motorcycles for rent
               </p>
             </Link>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
@@ -152,11 +151,11 @@ export default function Home() {
         {saleListings && saleListings.length > 0 && (
           <div className="m-2 mb-6">
             <h2 className="px-3 text-2xl mt-6 font-semibold">
-              Places for sale
+              Motorcycles for sale
             </h2>
             <Link to="/category/sale">
               <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
-                Show more places for sale
+                Show more motorcycles for sale
               </p>
             </Link>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
